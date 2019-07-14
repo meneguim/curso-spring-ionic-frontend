384ece32f0eb59409b44ad47bf250f761661f923
Alan Meneguim <alan.meneguim@gmail.com>
2019-07-14 18:22:46 -0300

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -13,6 +13,12 @@ export class ClienteService {
         ){
     }
 
+
+    findById(id: string) {
+        return this.http.get(
+            `${API_CONFIG.baseUrl}/clientes/${id}`);
+    }
+
     findByEmail(email: string) {
         return this.http.get(
             `${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
