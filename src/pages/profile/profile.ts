8f7f37558b294f48b6f33b82889f85c3f35ce3e2
Alan Meneguim <alan.meneguim@gmail.com>
2019-07-17 21:55:39 -0300

    Fanzendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -29,6 +29,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  } 
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email){
       this.clienteService.findByEmail(localUser.email)
@@ -45,7 +49,7 @@ export class ProfilePage {
     else {
       this.navCtrl.setRoot('HomePage');
     }
-  } 
+  }
 
   getImageIfExists(){
     this.clienteService.getImageFromBucket(this.cliente.id)
@@ -71,6 +75,20 @@ export class ProfilePage {
       this.cameraOn = false;
     }, (err) => {
     }); 
+  }
+
+  sendPicture(){
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      }, 
+      error => {
+      })
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
 
